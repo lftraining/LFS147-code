d7cc3d0968c8fd8c39083fb33ed88a39b96eb9bd
Chase Christensen <christensenc3526@gmail.com>
2024-02-26 05:29:13 +0000

    working containerized component

--- components/python/containerized/src/my_component.py
+++ components/python/containerized/src/my_component.py
@@ -4,8 +4,7 @@ from math_utils import calculate_area
 
 # Declare our base_image that our code will be packaged on top of and our target_image, which will be what we name our new container image when pushed to our registry
  
-@dsl.component(base_image='python:3.8',target_image='chasechristensen/material_cost_component:v1')
-
+@dsl.component(target_image='chasechristensen/material_cost_component:v1')
 #Declare our calculate_material_costs function 
 def calculate_material_cost(radius: float, cost_per_square_unit: float) -> float:
     """Calculates the cost of materials needed to cover a circle's area."""
